f48170bb29eeaec05701d0aa0a63fc7dc1f6c3bd
Karlking75 <kfn23003@student.mdu.se>
2024-05-22 10:31:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <Daniel_GPIO.h> //Ni behöver alltså inte inkludera er egna UARTE write, förutsätt att det finns en uarte_write, uarte_init osv osv
 #include "grupp1.h" //inkludera er header fil.
-
+#include "nonblocking.h" //Grupp 5
 /* By Daniel Morberg*/
 
 
@@ -17,6 +17,8 @@ int main(void)
 int keypress = get_key();
   switch(keypress){
     case 1:
+
+    case 5:
     //anrop till grupp 1's funktion
     breaK;
   //osv osv
